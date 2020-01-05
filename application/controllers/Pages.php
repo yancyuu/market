c70c5222dde4e89c1fd44356080334d99fcbe540
yancy <1191500820@qq.com>
2020-01-05 19:11:53 +0800

    对html文件做特殊处理

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -3,11 +3,16 @@ class Pages extends CI_Controller {
 
         public function view($page)
         {
-        if (!file_exists(APPPATH.'views/'.$page.'.html'))
-        {
+            //对html文件做特殊处理
+            if(substr($page,-5,5) == '.html')
+            {
+                $page = substr($page,0,-5);
+            }
+            if (!file_exists(APPPATH.'views/'.$page.'.html'))
+            {
                 // Whoops, we don't have a page for that!
                 show_404();
-        }
+            }
         $this->load->view($page.'.html');
         }
 }
